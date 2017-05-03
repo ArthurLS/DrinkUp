1095c1f3dc11de3911de58c629a244c7d01d7a9b
Arthur LE SAINT <arthur.lesaint@hotmail.fr>
2017-05-03 11:55:29 +0300

    Radio buttons

--- app/src/main/java/mobilesocial/drinkup/fragments/OneFragment.java
+++ app/src/main/java/mobilesocial/drinkup/fragments/OneFragment.java
@@ -1,8 +1,6 @@
 package mobilesocial.drinkup.fragments;
 
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,14 +9,11 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,7 +21,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-import mobilesocial.drinkup.Game_Settings;
 import mobilesocial.drinkup.R;
 
 public class OneFragment extends Fragment {
